7e30a64b9324492d0e118dd105da87d084170036
AYGJ <jofre1236@gmail.com>
2023-03-12 19:14:56 -0500

    Add a new file in public for error in production

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import CalendarApp from "./CalendarApp";
-import { Provider } from "react-redux";
-import { store } from "./store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   /*  <React.StrictMode> */
