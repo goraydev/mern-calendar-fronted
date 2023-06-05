6cb288e4f1b986a197228f54eab40a583d28a739
goraydev <jofre1236@gmail.com>
2023-06-05 11:24:40 -0500

    Update navbar for responsive design

--- src/calendar/components/NavBar.jsx
+++ src/calendar/components/NavBar.jsx
@@ -1,15 +1,25 @@
 import { useAuthStore } from "../../hooks";
 
 export const NavBar = () => {
-
-  const {startLogout, user} = useAuthStore();
+  const { startLogout, user } = useAuthStore();
 
   return (
     <nav className="bg-slate-800 py-5">
-      <div className="container mx-auto flex justify-between">
+      <div className="mx-10 flex justify-between">
         <div className="flex items-center gap-1">
-          <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-calendar-event" width="20" height="20" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#ffffff" fill="none" strokeLinecap="round" strokeLinejoin="round">
-            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="icon icon-tabler icon-tabler-calendar-event"
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="#ffffff"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
             <rect x="4" y="5" width="16" height="16" rx="2" />
             <line x1="16" y1="3" x2="16" y2="7" />
             <line x1="8" y1="3" x2="8" y2="7" />
@@ -18,12 +28,26 @@ export const NavBar = () => {
           </svg>
           <span className="text-white">{user.name}</span>
         </div>
-        <button className="text-white flex items-center gap-1 border border-transparent hover:border-red-500" onClick={startLogout}>
-        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-logout" width="20" height="20" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#ffffff" fill="none" strokeLinecap="round" strokeLinejoin="round">
-          <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-          <path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2" />
-          <path d="M7 12h14l-3 -3m0 6l3 -3" />
-        </svg>
+        <button
+          className="text-white flex items-center gap-1 border border-transparent hover:border-red-500"
+          onClick={startLogout}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="icon icon-tabler icon-tabler-logout"
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="#ffffff"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2" />
+            <path d="M7 12h14l-3 -3m0 6l3 -3" />
+          </svg>
           Log out
         </button>
       </div>
